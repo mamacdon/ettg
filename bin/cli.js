6c1b5f585d56566c032440c3187c3a5f68f4ed18
Mark Macdonald <mamacdon@gmail.com>
2016-11-19 17:25:38 -0500

    CLI works

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 /*eslint-disable no-console*/
 var generator = require("../lib/generator")();
 
+console.log("NOW PLAYING:\n");
 for (var i = 0; i < 10; i++) {
-    console.log(generator.get())
+    console.log(" * " + generator.getString())
 }
