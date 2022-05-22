490dff2bcc6f2e1e09c38486dad59deba2147730
mamacdon <mamacdon@gmail.com>
2022-05-22 18:28:58 -0400

    refactor - extract fs, path dependencies
    - convert to esm
    
    - remove cryto b/c its more trouble than its worth

--- bin/cli.js
+++ bin/cli.js
@@ -2,8 +2,13 @@
 
 /*eslint-env node*/
 /*eslint-disable no-console*/
-var Generator = require("../lib/generator");
-var gen = new Generator();
+import Generator from "../lib/generator.js";
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+const gen = new Generator({
+    loadRules: () => readFileSync(resolve("./rules.txt"), "utf8")
+});
 
 for (var i = 0; i < 1500; i++) {
     console.log("" + gen.getString())
--- bin/debug.js
+++ bin/debug.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 
-const generator = require("../lib/generator")();
-console.log(generator.grammar.toString());
+import Generator from "../lib/generator.js";
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+const gen = new Generator({
+    loadRules: () => readFileSync(resolve("./rules.txt"), "utf8")
+});
+
+console.log(gen.grammar.toString());
